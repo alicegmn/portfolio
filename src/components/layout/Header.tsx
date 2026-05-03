46823d79509a604bfbb5134c2e295dc4a5569963
alicegmn <243100080+alicegmn-dd@users.noreply.github.com>
2026-05-03 21:05:42 +0200

    fix: project clean up

--- src/components/layout/Header.tsx
+++
@@ -1,9 +0,0 @@
-import Nav from "@/components/layout/Nav";
-
-export default function Header() {
-  return (
-    <header className="w-full flex justify-center bg-white shadow-md py-4 top-0 z-50">
-      <Nav />
-    </header>
-  );
-}
